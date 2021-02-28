73c07f8262713e897bedbcccda999abd1ca2ece6
Anatoliy Kigel <anatoliy.kigel@gmail.com>
2021-02-28 20:29:03 +0200

    Update

--- src/template-nbu/assets/js/main.js
+++ src/template-nbu/assets/js/main.js
@@ -1,40 +1,21 @@
 
     const URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/ovdp?json';
-   
-    fetch(URL)
-        .then(answer => answer.json())
-        .then(data => {
 
-            /*
-                В середине этой функции вам доступна переменная data
-                хранящая данные по размещенным облигациям внутреннего займа
-            */
 
-            const today = '2021-02-23';
+    let xhr = new XMLHttpRequest();
 
-            data = data.map(item => ({
+    xhr.open('GET', URL);
 
-                d: item.repaydate.split('.').reverse().join('-'),
-                s: item.valcode == 'USD' 
-                    ? item.attraction * 28 
-                    : (item.valcode == 'EUR' 
-                        ? item.attraction * 33 
-                        : item.attraction)
+    xhr.onload = function(){
 
-            })).filter(item => item.d > today && item.s).reduce((acc, item) => {
-                acc[item.d] = acc[item.d] ? acc[item.d] + item.s : item.s;
-                return acc; 
-            }, {});
+        let data = JSON.parse(xhr.responseText);
 
-            data = Object.entries(data).sort((a, b) => a[0] > b[0] ? 1 : -1);
+        console.log(data);
 
-            
-            console.log(data);
+        /* Далее, в этой функции, вам доступна переменная data с загруженными данными */
 
-            
-             //outputPlace.innerHTML = '...Result...';
+        /* Конец функции обработки события загрузки данных */
+    }
 
-        });
-
-   
-	
+    xhr.send();
+  
